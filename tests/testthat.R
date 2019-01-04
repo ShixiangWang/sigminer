3bd31644c507c229dd60bf6be9a02cd54e948e23
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-01-05 00:22:17 +0800

    use testthat and codecov

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(sigminer)
+
+test_check("sigminer")
---
+++ tests/testthat/test-reading_data.R
@@ -0,0 +1,19 @@
+context("test-reading_data")
+
+extdata_dir = system.file("extdata", package = "sigminer", mustWork = TRUE)
+laml.maf <- system.file("extdata", "tcga_laml.maf.gz", package = "maftools", mustWork = TRUE)
+
+test_that("read copy number or maf work", {
+  skip_on_cran()
+
+  cp = read_copynumber(extdata_dir, pattern = "txt", genome_build = "hg19")
+  expect_s4_class(cp, "CopyNumber")
+
+  laml = read_maf(maf = laml.maf)
+  expect_s4_class(laml, "MAF")
+
+  vt = read_variation(cp,  laml)
+  expect_s4_class(vt, "GenomicVariation")
+})
+
+extdata_dir <- laml.maf <- cp <- laml <- vt <- NULL
