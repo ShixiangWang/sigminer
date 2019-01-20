fa8a5bab0c598b4bbf70d8b77eef46ee4eb3ab7b
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-01-21 00:02:08 +0800

    init pkg doc

---
+++ R/sigminer.R
@@ -0,0 +1,16 @@
+#' sigminer: Capture Genomic Variation Signatures using Non-Negative Matrix Factorization
+#'
+#' sigminer provides functions for identification of
+#' copy number signatures (Geoffrey et al. (2018) <doi:10.1038/s41588-018-0179-8>) and
+#' mutation signatures (Alexandrov et al. (2018) <doi:10.1038/nature12477>)
+#' by non-negative matrix factorization, signature analysis and visualization.
+#' It can be used to capture signatures of genomic variation, compare genotype or
+#' phenotype features of different signatures and thus uncover the relationship
+#' between the mechanism of genomic variation and phenotypes in cancer.
+#'
+#' @section read_ functions:
+#' read_ functions read data into R objects.
+#'
+#' @docType package
+#' @name sigminer
+NULL
