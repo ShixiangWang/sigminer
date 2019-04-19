882c628d7b148790ec6261ff52dd1d2e24b588bd
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-04-19 22:46:48 +0800

    👌 IMPROVE: modify citation

--- R/hello.R
+++ R/hello.R
@@ -14,12 +14,22 @@ Github page: https://github.com/ShixiangWang/sigminer
 Bug report: https://github.com/ShixiangWang/sigminer/issues
 Documentation: https://github.com/ShixiangWang/sigminer
 
+==
 If you use 'sigminer' in academic field, please cite:
-Macintyre, Geoff, et al. \"Copy number signatures and mutational
-    processes in ovarian carcinoma.\" Nature genetics 50.9 (2018): 1262.
+
 Wang, Shixiang, et al. \"APOBEC3B and APOBEC mutational signature
     as potential predictive markers for immunotherapy
     response in non-small cell lung cancer.\" Oncogene (2018).
+==
+Also cite the following if you extract copy number signatures
+
+Macintyre, Geoff, et al. \"Copy number signatures and mutational
+    processes in ovarian carcinoma.\" Nature genetics 50.9 (2018): 1262.
+==
+Also cite the following if you extract mutational signatures
+
+Mayakonda, Anand, et al. \"Maftools: efficient and comprehensive analysis
+    of somatic variants in cancer.\" Genome research 28.11 (2018): 1747-1756.
 =========================================================================
                  ")
     cowsay::say(msg, by = "random")
