43e69f488a66dae67404e924a6b13427f389256d
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-09-23 18:12:27 +0800

    Update test.R

--- inst/test/test.R
+++ inst/test/test.R
@@ -6,3 +6,15 @@ cn_estimate <- sig_estimate(cn_prepare$nmf_matrix,
                             cores = 1, nrun = 5,
                             verbose = TRUE
 )
+
+
+# Rewriting show cn components --------------------------------------------
+
+# Load copy number prepare object
+load(system.file("extdata", "toy_copynumber_prepare.RData",
+                 package = "sigminer", mustWork = TRUE
+))
+show_cn_components(cn_prepare$features, cn_prepare$components)
+
+data = cn_prepare$parameters
+
