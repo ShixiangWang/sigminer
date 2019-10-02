92908b5b04152ae6ff835a28a2159e30d3466752
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2019-10-02 15:25:28 +0800

    Fix #47

--- R/sig_estimate.R
+++ R/sig_estimate.R
@@ -155,7 +155,7 @@ sig_estimate <-
     print(p)
 
     if (save_plots) {
-      destdir <- basename(plot_basename)
+      destdir <- dirname(plot_basename)
       if (!dir.exists(destdir)) dir.create(destdir, recursive = TRUE)
       pdf(
         paste0(plot_basename, "_survey.pdf"),
