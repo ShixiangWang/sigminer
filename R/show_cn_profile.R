9e7e7b70afed73bd86f8ea7187b2042039c50469
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-11-06 01:06:50 +0800

    Finish basic profile plot

--- R/show_cn_profile.R
+++ R/show_cn_profile.R
@@ -64,16 +64,43 @@ show_cn_profile = function(data, samples=NULL,
                     .data$segVal < 2 ~ "Del"
                   ))
 
-  plot_df = merge_df %>% dplyr::filter(sample == "TCGA-A8-A07S-01A-11D-A036-01")
 
-  ggplot() +
-    geom_segment(aes(x = .data$start, xend = .data$end,
-                     y = .data$segVal, yend = .data$segVal,
-                     color=.data$segType), data = plot_df) +
-    geom_vline(aes(xintercept = .data$x_start), linetype="dotted", data = coord_df) +
-    geom_vline(xintercept = coord_df$x_end[nrow(coord_df)], linetype="dotted") +
-    scale_x_continuous(breaks = coord_df$lab_loc, labels = coord_df$labels) +
-    scale_color_manual(values = c("Amp"="red", "Normal"="black", "Del"="blue"))
+  plot_cn_profile = function(plot_df, coord_df) {
+    ggplot() +
+      geom_segment(aes(x = .data$start, xend = .data$end,
+                       y = .data$segVal, yend = .data$segVal,
+                       color=.data$segType), data = plot_df) +
+      geom_vline(aes(xintercept = .data$x_start), linetype="dotted", data = coord_df) +
+      geom_vline(xintercept = coord_df$x_end[nrow(coord_df)], linetype="dotted") +
+      scale_x_continuous(breaks = coord_df$lab_loc, labels = coord_df$labels) +
+      scale_color_manual(values = c("Amp"="red", "Normal"="black", "Del"="blue")) +
+      labs(x = "Chromosome", y = "Copy Number") +
+      theme(
+        legend.position = "none"
+      )
+  }
+
+
+  if (!'sample' %in% colnames(merge_df)) {
+    # Plot as a single sample
+    gg = plot_cn_profile(merge_df, coord_df)
+  } else {
+    # Plot mutiple samples
+    gg_df = merge_df %>%
+      dplyr::group_by(.data$sample) %>%
+      tidyr::nest() %>%
+      dplyr::mutate(gg = purrr::map(.data$data,
+                                    plot_cn_profile,
+                                    coord_df=coord_df))
+    gg_list = gg_df$gg
+    names(gg_list) = gg_df$sample
+    if (return_plotlist) {
+      return(gg_list)
+    }
+    gg = cowplot::plot_grid(plotlist = gg_list, align = "hv",
+                            nrow = nrow, ncol = ncol)
+  }
+  return(gg)
 }
 
 
