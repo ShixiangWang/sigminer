e3bb074a2ff0c04ff81effb82e78c38c145b460f
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-12-15 17:49:11 +0800

    Modify sort method

--- R/helper_sort_signature.R
+++ R/helper_sort_signature.R
@@ -2,20 +2,21 @@ helper_sort_signature = function(sig) {
   stopifnot(is.matrix(sig), !is.null(rownames(sig)))
   # Component names are shown as rownames
   # Sort mutational signatures by C>T mutation type
-  # Sort copy number signatures by BP10MB feature
+  # Sort copy number signatures by fraction of the first component of BP10MB feature
   is_c2t = grepl("C>T", rownames(sig))
   if (any(is_c2t)) {
-    mat_colsum <- colSums(sig[is_c2t, ])
+    to_rank <- colSums(sig[is_c2t, ])
   } else {
     use_M <- any(grepl("\\d+$", rownames(sig)))
     if (use_M) {
-    mat_colsum <- colSums(sig[startsWith(rownames(sig), "bp10MB"), ])
+      mat <- sig[startsWith(rownames(sig), "bp10MB"), , drop = FALSE]
+      to_rank <- mat[1, ] / colSums(mat)
     } else {
       # Method: Wang
-      mat_colsum <- colSums(sig[startsWith(rownames(sig), "BP10MB"), ])
+      mat <- sig[startsWith(rownames(sig), "BP10MB"), , drop = FALSE]
+      to_rank <- mat[1, ] / colSums(mat)
     }
   }
-  sig_order <- rank(mat_colsum)
+  sig_order <- rank(to_rank)
   return(sig_order)
-
 }
