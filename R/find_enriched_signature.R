d8d3b860b18c9fd5be10729a729cb49d09a4f33b
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-12-23 19:28:41 +0800

    Handle hyper-mut samples

--- R/find_enriched_signature.R
+++ R/find_enriched_signature.R
@@ -1,5 +1,29 @@
 # Find Enriched Signature in Each Sample Group Based on Its Dominant Signature (Weighted by Exposure)
 find_enriched_signature <- function(group_df, Signature) {
+
+  # Handle hyper mutant samples
+  hyper_index <- grepl("_\\[hyper\\]_", group_df$sample)
+  if (sum(hyper_index) > 0) {
+    sample.hyper <- sapply(
+      group_df$sample,
+      function(x) strsplit(x, "_\\[hyper\\]_")[[1]][[1]]
+    )
+
+    df <- dplyr::tibble(
+      sample = names(sample.hyper),
+      ID = as.character(sample.hyper)
+    )
+
+    group_df <- dplyr::left_join(group_df, df, by = "sample") %>%
+      dplyr::select(-.data$sample) %>%
+      dplyr::rename(sample = .data$ID) %>%
+      dplyr::group_by(.data$sample) %>%
+      dplyr::summarise(
+        group = table(.data$group) %>% sort(decreasing = TRUE) %>% names() %>% .[[1]],
+        silhouette_width = mean(.data$silhouette_width)
+      )
+  }
+
   exposure_df <- get_sig_exposure(Signature = Signature, type = "relative")
   merge_df <- dplyr::left_join(group_df, exposure_df, by = "sample")
   ztable <- merge_df %>%
