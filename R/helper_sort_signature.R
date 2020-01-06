0501520b511bc3ab53f8e31ce60492ac163a64b7
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-01-06 21:32:21 +0800

    try sorting by segsize

--- R/helper_sort_signature.R
+++ R/helper_sort_signature.R
@@ -2,22 +2,49 @@ helper_sort_signature <- function(sig) {
   stopifnot(is.matrix(sig), !is.null(rownames(sig)))
   # Component names are shown as rownames
   # Sort mutational signatures by C>T mutation type
-  # Sort copy number signatures by fraction of the first component of BP10MB feature for method 'M'
-  # Sort copy number signatures by fraction of the copy number value 2 for method 'W'
+  # Sort copy number signatures by segsize/SS feature for method 'M'
   is_c2t <- grepl("C>T", rownames(sig))
   if (any(is_c2t)) {
     to_rank <- colSums(sig[is_c2t, ])
+    sig_order <- order(to_rank)
   } else {
     use_M <- any(grepl("\\d+$", rownames(sig)))
     if (use_M) {
-      mat <- sig[startsWith(rownames(sig), "bp10MB"), , drop = FALSE]
-      to_rank <- mat[1, ] / colSums(mat)
+      mat <- sig[startsWith(rownames(sig), "segsize"), , drop = FALSE]
     } else {
       # Method: Wang
-      mat <- sig[startsWith(rownames(sig), "CN"), , drop = FALSE]
-      to_rank <- mat[3, ] / colSums(mat)
+      mat <- sig[startsWith(rownames(sig), "SS"), , drop = FALSE]
     }
+    sig_order <- get_segsize_order(mat)
   }
-  sig_order <- order(to_rank)
   return(sig_order)
+}
+
+# Matrix: feature-by-signature
+get_segsize_order = function(mat) {
+  index = apply(mat, 2, which.max)
+  is_unique = length(index) == length(unique(index))
+  if (!is_unique) {
+    # Compare value with same index
+    mat %>%
+      t() %>%
+      dplyr::as_tibble() %>%
+      dplyr::mutate(N = dplyr::row_number(),
+                    index = index) %>%
+      dplyr::arrange(.data$index) %>%
+      dplyr::group_by(.data$index) %>%
+      tidyr::nest() %>%
+      dplyr::mutate(data = purrr::map2(.data$data, .data$index, .f = function(x, y) {
+        if (nrow(x) == 1) {
+          return(x$N)
+        } else {
+          N = x$N[order(x[[y]])]
+          return(N)
+        }
+      })) %>%
+      tidyr::unnest("data") %>%
+      dplyr::pull("data")
+  } else {
+    return(order(index))
+  }
 }
