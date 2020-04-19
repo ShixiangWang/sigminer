77f59103d2e692df04d99dcd7fd0f116c59e160f
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-04-20 00:23:06 +0800

    Add strand bias test

--- R/generate_matrices.R
+++ R/generate_matrices.R
@@ -320,6 +320,77 @@ vector_to_combination <- function(...) {
     unique()
 }
 
+get_component_strand_bias = function(mat) {
+  stopifnot(is.matrix(mat))
+
+  ## mat: sample-by-component matrix with T: and U: labels
+  T_cols = startsWith(colnames(mat), "T:")
+  U_cols = startsWith(colnames(mat), "U:")
+
+  if (!any(T_cols) & !any(U_cols)) {
+    stop("No transcription labels found!")
+  }
+
+  types = colnames(mat)[T_cols]
+  types = sub("T:", "", types)
+  mat = mat[, T_cols | U_cols]
+
+  dt = dplyr::bind_cols(dplyr::tibble(sample = rownames(mat)),
+                        dplyr::as_tibble(mat))
+
+  f4row = function(df) {
+
+    df = df %>%
+      tidyr::pivot_longer(dplyr::contains(":"),
+                          names_sep = ":",
+                          names_to = c("strand", "component"),
+                          values_to = "count") %>%
+      dplyr::mutate(
+        strand = ifelse(.data$strand == "T", "Trans", "UnTrans")
+      ) %>%
+      tidyr::pivot_wider(id_cols = "component",
+                         names_from = "strand",
+                         values_from = "count") %>%
+      dplyr::mutate(Trans_Total = sum(.data$Trans),
+                    UnTrans_Total = sum(.data$UnTrans),
+                    Enrichment = .data$Trans / .data$UnTrans)
+
+
+    df$p_value =apply(df, 1, function(x) {
+      x = as.numeric(x[2:5])
+      # The order is right?
+      # https://shixiangwang.gitee.io/cookbook-for-r-chinese/section-7.html#section-7.3
+      fisher.test(
+        matrix(
+          c(x[1], x[2],
+            x[3], x[4]),
+          nrow = 2
+        )
+      )$p.value
+    })
+    df$fdr = p.adjust(df$p_value, method = "fdr")
+
+    return(df)
+  }
+
+  res = dt %>%
+    dplyr::group_by(.data$sample) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(
+      data = list(
+        purrr::map_df(
+          .data$data,
+          f4row
+        )
+      )
+    ) %>%
+    tidyr::unnest("data") %>%
+    dplyr::arrange(.data$fdr, .data$p_value)
+
+  return(res)
+}
+
+
 utils::globalVariables(
   c(
     "A", "APOBEC_Enriched", "APOBEC_Enrichment",
