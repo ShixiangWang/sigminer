b05c6167793b8ac0726b20cb6677bc108923e30e
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-11-14 22:02:46 +0800

    Create best_practice.R

---
+++ R/best_practice.R
@@ -0,0 +1,6 @@
+# A Best Practice for Signature Extraction and Activity (Exposure) Attribution
+# 先基于已知的 signature 进行拟合，然后再提取额外的未知signature也是一种可行方式
+
+# 当两个随机分布相同时，它们的相对熵为零，当两个随机分布的差别增
+# 大时，它们的相对熵也会增大。
+# deviance(res)
