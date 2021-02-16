5ef6d669536575dc8f09fe31a4a82af155bf81ec
taoziyu97 <baomihai@sina.com>
2021-02-16 15:18:23 +0800

    drop unused data

--- inst/test/test_get_sv.R
+++ inst/test/test_get_sv.R
@@ -1,8 +1,4 @@
-# sv <- readxl::read_xlsx("inst/extdata/sv.xlsx") %>%
-#   as.data.frame()
-# save(sv, file = "./inst/extdata/sv.RData")
-
-load(system.file("extdata","sv.RData",package = "sigminer",mustWork = TRUE))
+load("data-raw/sv.RData")
 sv_dt <- read_sv(input = sv)
 # usethis::use_package("copynumber", "Suggests")
 sv_sig_tally <- sig_tally.SV(sv_dt)
